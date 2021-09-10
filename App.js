2525e7673536e3bb5643f782bb5aac55a68e3975
Dragoș-Andrei NIȚĂ (67189) <dragos_andrei.nita@stud.acs.upb.ro>
2021-09-10 18:45:34 +0300

    Added theme colors

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import { NativeBaseProvider, Box } from "native-base";
+import { NativeBaseProvider, Text, Box, extendTheme } from "native-base";
+const theme = extendTheme({
+  colors: {
+    primary: {
+      50: "#dcfffd",
+      100: "#b3faf4",
+      200: "#87f5eb",
+      300: "#5bf1e3",
+      400: "#33ecdb",
+      500: "#1fd3c2",
+      600: "#0ea497",
+      700: "#00756c",
+      800: "#004741",
+      900: "#001917",
+    },
+  },
+});
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Box>Hello world</Box>
+    <NativeBaseProvider theme={theme}>
+      <Box
+        flex={1}
+        bg="primary.200"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Text>Open up App.js to start working on your app!</Text>
+      </Box>
     </NativeBaseProvider>
   );
 }
