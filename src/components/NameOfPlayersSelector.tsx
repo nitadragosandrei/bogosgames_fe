e0767bc205d8acf752477078a77cc44dff726f38
nitadragosandrei <nitadragosandrei@gmail.com>
2021-11-02 13:02:18 +0200

    Butonul next sa treaca la urmatoarea pagina

--- src/components/NameOfPlayersSelector.tsx
+++ src/components/NameOfPlayersSelector.tsx
@@ -1,20 +1,40 @@
-import { Input, VStack, Button, Heading } from "native-base";
+import { Input, VStack, Button, Heading, FormControl } from "native-base";
 import React from "react";
 function NameOfPlayersSelector(props) {
+  const [players, setPlayers] = React.useState({});
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setPlayers({
+      ...players,
+      [e.target.id]: value,
+    });
+  }
+
   return (
     <VStack alignItems="center" space={4}>
       <Heading textAlign="center" mb="10">
         Insert names
       </Heading>
-      {[...Array(props.numberOfPlayers).keys()].map((index) => {
-        return (
-          <Input
-            size="xs"
-            placeholder={"Insert player " + (index + 1) + " name"}
-          />
-        );
-      })}
-      <Button>Next</Button>
+      <FormControl>
+        {[...Array(props.numberOfPlayers).keys()].map((index) => {
+          return (
+            <Input
+              nativeID={"player" + index}
+              size="xs"
+              placeholder={"Insert player " + (index + 1) + " name"}
+              onChange={handleChange}
+            />
+          );
+        })}
+      </FormControl>
+      <Button
+        onPress={() => {
+          console.log(players);
+        }}
+      >
+        Next
+      </Button>
     </VStack>
   );
 }
