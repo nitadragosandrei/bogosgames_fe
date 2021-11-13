c5762e502ef2659ef8c169215b0fd8ea7d7f70d1
Andrei Dragos Dobrescu <andreidobrescu42@gmail.com>
2021-11-13 13:37:19 +0200

    Work in progress - butoanele cu playeri sa fie disabled dupa click pe ele in timpul rundei

--- src/components/Presedintii.tsx
+++ src/components/Presedintii.tsx
@@ -17,11 +17,22 @@ import NameOfPlayersSelector from "./NameOfPlayersSelector";
 import ScoreTable from "./ScoreTable";
 
 function Presedintii({ navigation, route }) {
-  const [state, setState] = React.useState();
+  const [playerButton, setPlayerButton] = React.useState(route.params.players.index);
+
   const [roundOngoing, setroundOngoing] = React.useState(false);
   const [gameOngoing, setgameOngoing] = React.useState(true);
   const [points, setPoints] = React.useState(route.params.numberOfplayers);
   const [showModal, setShowModal] = React.useState(false);
+
+
+  function handlePress(e) {
+    setPlayerButton({
+      ...playerButton,
+      [e.currentTarget.id]: {isDisabled: true},
+    });
+    console.log(e.target.id)
+  }
+
 
   return (
     <Flex alignItems="center">
@@ -29,13 +40,17 @@ function Presedintii({ navigation, route }) {
         <Button.Group direction="column" variant="outline">
           {Object.values(route.params.players).map((player, index) => (
             <Button
+              nativeID={String(index)}
               key={index}
               onPressIn={() => {
                 player["score"] += points;
                 if (points === 0) setroundOngoing(false);
                 setPoints(points - 1);
                 console.log(route.params.players);
+                console.log(playerButton)
               }}
+              onPress={handlePress}
+              isDisabled={playerButton.index.isDisabled ? true : false }
             >
               {player["name"]}
             </Button>
