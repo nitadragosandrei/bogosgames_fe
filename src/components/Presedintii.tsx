d3a89300ac51d1b33f46df74a6e840d98bab9a54
nitadragosandrei <nitadragosandrei@gmail.com>
2021-11-13 11:54:30 +0200

    M-am cacat de doua ori azi

--- src/components/Presedintii.tsx
+++ src/components/Presedintii.tsx
@@ -1,4 +1,15 @@
-import { Alert, Button, Flex, Image, Modal, Text, View } from "native-base";
+import {
+  Alert,
+  Button,
+  Flex,
+  Image,
+  Modal,
+  Text,
+  View,
+  HStack,
+  Fab,
+  NativeBaseProvider,
+} from "native-base";
 import React from "react";
 import { BackHandler, Pressable } from "react-native";
 import NumberOfPlayersSelector from "./NumberOfPlayersSelector";
@@ -6,21 +17,41 @@ import NameOfPlayersSelector from "./NameOfPlayersSelector";
 import ScoreTable from "./ScoreTable";
 
 function Presedintii({ navigation, route }) {
-  const [roundOngoing, setroundOngoing] = React.useState(true);
+  const [roundOngoing, setroundOngoing] = React.useState(false);
   const [gameOngoing, setgameOngoing] = React.useState(true);
+  const [points, setPoints] = React.useState(route.params.numberOfplayers);
   const [showModal, setShowModal] = React.useState(false);
 
   return (
     <Flex alignItems="center">
-      <Image
-        size="2xl"
-        resizeMode="center"
-        source={{
-          uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Cards-3-Club.svg/1200px-Cards-3-Club.svg.png",
-        }}
-        alt={"Treiul de trefla"}
-      />
+      {roundOngoing ? (
+        <Button.Group direction="column" variant="outline">
+          {Object.values(route.params.players).map((player, index) => (
+            <Button
+              key={index}
+              onPressIn={() => {
+                console.log(player["name"] + " are pula medie ");
+              }}
+            >
+              {player["name"]}
+            </Button>
+          ))}
+        </Button.Group>
+      ) : (
+        <Pressable onPress={() => setroundOngoing(true)}>
+          <Image
+            size="2xl"
+            resizeMode="center"
+            source={{
+              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Cards-3-Club.svg/1200px-Cards-3-Club.svg.png",
+            }}
+            alt={"Treiul de trefla"}
+          />
+        </Pressable>
+      )}
+
       <Button onPress={() => setShowModal(true)}>Vezi scorul</Button>
+
       <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
         <Modal.Content maxWidth="400px">
           <Modal.CloseButton />
